504999da543774ad44e01d33ba6cdc1fc6603b19
Trushhackyou <50118284+Trushhackyou@users.noreply.github.com>
2021-06-05 15:48:11 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+           <h1> <center><u>ShapeAI BootCamp</u></center></h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>
+                Javascript and React
+            </h1>
+            <p>
+                THis was an amazing bootcamp conducted by Shaurya Sinha from ShapeAI, I have attended so far...
+            </p>
+        </div>
+    );
+}
+
+export default Note;
